aee07f793014c7dd8ddb1857b793cb2e18f19f46
Paul Belanger <paul.belanger@polybeacon.com>
2014-04-13 21:14:46 -0400

    Import into kickstandproject-infra
    Change-Id: Ic085cad8049ca6e6db1c73ab30c97b05ed2be7ab
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+# == Class: apt
+#
+# === Authors
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+# === Copyright
+#
+# Copyright (C) 2012-2014, PolyBeacon, Inc.
+#
+# This program is free software, distributed under the terms
+# of the Apache License, Version 2.0. See the LICENSE file at
+# the top of the source tree.
+#
+class apt {
+}
+
+# vim:sw=2:ts=2:expandtab
